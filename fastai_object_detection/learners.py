d054dab82f6384cf00d71271fff5a2a0a346bd81
Robert Walz <abitdevelopment@gmail.com>
2021-08-03 23:00:38 +0200

    get_preds and show_results docstring

--- fastai_object_detection/learners.py
+++ fastai_object_detection/learners.py
@@ -43,7 +43,7 @@ class ObjDetLearner(Learner): pass
 @patch
 def get_preds(x:ObjDetLearner, items=None, item_tfms=None, batch_tfms=None,
               box_score_thresh=0.05, max_n=None, progress=True):
-    """Get predictions of an `ObjDetLearner`. Set items to a list of PIL images, optionally with
+    """Get predictions of an `ObjDetLearner`. Set `items` to a list of PIL images, optionally with
     item and batch transforms. Returns denormalized inputs, bounding boxes, labels and scores as lists of
     tensors.
     """
@@ -95,7 +95,7 @@ def get_preds(x:ObjDetLearner, items=None, item_tfms=None, batch_tfms=None,
 @patch
 def show_results(x:ObjDetLearner, items=None, item_tfms=None, batch_tfms=None,
                  box_score_thresh=0.50, max_n=5, progress=False):
-    """Show results of a trained `ObjDetLearner`. If no items are specified, it uses images from
+    """Show results of a trained `ObjDetLearner`. If no `items` are specified, it uses images from
     the validation dataloader."""
 
     inputs, boxes, labels, scores = x.get_preds(items=items, item_tfms=item_tfms, batch_tfms=batch_tfms,
@@ -119,7 +119,7 @@ def get_preds(x:InstSegLearner, items=None, item_tfms=None, batch_tfms=None,
               box_score_thresh=0.05, bin_mask_thresh=None, max_n=None, progress=True):
     """Get predictions of an `InstSegLearner`. Set `items` to a list of PIL images, optionally with
     item and batch transforms. Returns denormalized inputs, masks, bounding boxes, labels and scores as lists of
-    tensors. By default it returns masks in [N,1,H,W] with actications. If you want binary masks in [N,H,W],
+    tensors. By default it returns masks in `[N,1,H,W]` with activations. If you want binary masks in `[N,H,W]`,
     set a value for `bin_mask_thresh` (N: number of objects).
     """
 
@@ -177,7 +177,7 @@ def get_preds(x:InstSegLearner, items=None, item_tfms=None, batch_tfms=None,
 @patch
 def show_results(x:InstSegLearner, items=None, item_tfms=None, batch_tfms=None,
                  box_score_thresh=0.50, bin_mask_thresh=0.50, max_n=5, progress=False):
-    """Show results of a trained `InstSegLearner`. If no items are specified, it uses images from
+    """Show results of a trained `InstSegLearner`. If no `items` are specified, it uses images from
     the validation dataloader."""
 
     inputs, masks, boxes, labels, scores = x.get_preds(items=items, item_tfms=item_tfms, batch_tfms=batch_tfms,
